aebc33a6f06e05c7e735fd32c2e28a13a68f31cb
comdotlinux <guru@linux.com>
2016-02-27 12:55:41 +0530

    updated to string

--- src/main/java/com/linux/rhcloud/javaee/movieinfo/business/entity/Actor.java
+++ src/main/java/com/linux/rhcloud/javaee/movieinfo/business/entity/Actor.java
@@ -14,7 +14,6 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
 
 /**
