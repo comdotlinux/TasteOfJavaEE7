37a483d502f1155f6d45b3d865b031e9df96ab92
Guruprasad Kulkarni <guru@linux.com>
2016-03-03 09:56:54 +0530

    Corss Check Validation Not Working.

--- src/main/java/com/linux/rhcloud/javaee/movieinfo/presentation/ActorView.java
+++ src/main/java/com/linux/rhcloud/javaee/movieinfo/presentation/ActorView.java
@@ -5,27 +5,31 @@ import com.linux.rhcloud.javaee.movieinfo.business.actor.entity.Actor;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import org.slf4j.Logger;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Backing bean for ActorView.xhtml
+ *
  * @author Guruprasad Kulkarni <guru@linux.com>
  */
 @Model
 public class ActorView {
-     private static final Logger LOG = getLogger(ActorView.class);
-    
-     private Actor actor;
-    
-     @Inject
-     private ActorManager actorManager;
-     
-     @PostConstruct
-     public void init(){
-         this.actor = new Actor();
-     }
+
+    private static final Logger LOG = getLogger(ActorView.class);
+
+    private Actor actor;
+
+    @Inject
+    private ActorManager actorManager;
+
+    @PostConstruct
+    public void init() {
+        this.actor = new Actor();
+    }
 
     public Actor getActor() {
         return actor;
@@ -34,17 +38,19 @@ public class ActorView {
     public void setActor(Actor actor) {
         this.actor = actor;
     }
-    
-    public Object addActor(){
-        LOG.info("Actor is {}",this.actor);
+
+    public Object addActor() {
+        LOG.info("Adding Actor {}", this.actor);
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Cross Field Validation Failed", "Cross Field Validation Failed");
+        FacesContext.getCurrentInstance().addMessage(null, message);
         this.actorManager.add(this.actor);
         return null;
     }
-    
-    public List<Actor> getActors(){
+
+    public List<Actor> getActors() {
         final List<Actor> allActors = this.actorManager.all();
         LOG.info("{} Actors retrieved.", allActors.size());
         return allActors;
     }
-     
+
 }
