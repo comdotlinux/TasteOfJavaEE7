d274ea56b10966a223f99cad8b3be86ed302f47c
Guruprasad Kulkarni <guru@linux.com>
2016-03-11 10:26:16 +0530

    Added Integration Test Dependencies

---
+++ src/test/java/com/rhcloud/javaee/movieinfo/business/actor/boundry/ActorResourceIT.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rhcloud.javaee.movieinfo.business.actor.boundry;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Guruprasad Kulkarni <guru@linux.com>
+ */
+public class ActorResourceIT {
+    
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
