255f8ca6b9eb761c1b4d16cb276b6f35f1a5be91
Guruprasad Kulkarni <guru@linux.com>
2016-03-15 08:54:02 +0530

    Added Integration Test Basics -- no asserts

--- src/test/java/com/rhcloud/javaee/movieinfo/business/actor/boundry/ActorResourceIT.java
+++ src/test/java/com/rhcloud/javaee/movieinfo/business/actor/boundry/ActorResourceIT.java
@@ -26,17 +26,8 @@ public class ActorResourceIT {
     @Rule
     public JAXRSClientProvider provider = JAXRSClientProvider.buildWithURI(URI);
     
-    @Before
-    public void setUp() {
-    }
     
-    @After
-    public void tearDown() {
-    }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+     @Test
+     public void hello() {
+     }
 }
