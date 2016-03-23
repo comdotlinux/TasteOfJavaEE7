cbbb1ac2d420456c8c35543794bc2270b39959fa
Guruprasad Kulkarni <guru@linux.com>
2016-03-23 14:19:01 +0530

    Working infinite scrolling for films -- updates

--- src/main/java/com/linux/rhcloud/javaee/movieinfo/business/film/entity/Film.java
+++ src/main/java/com/linux/rhcloud/javaee/movieinfo/business/film/entity/Film.java
@@ -5,8 +5,6 @@ import java.math.BigDecimal;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
