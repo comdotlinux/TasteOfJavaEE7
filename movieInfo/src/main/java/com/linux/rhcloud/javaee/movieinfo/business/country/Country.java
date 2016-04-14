80604552a15065f3627da249ef18b0df0962406e
Guruprasad Kulkarni <guru@linux.com>
2016-04-14 09:41:43 +0530

    Updating for Address further

---
+++ movieInfo/src/main/java/com/linux/rhcloud/javaee/movieinfo/business/country/Country.java
@@ -0,0 +1,96 @@
+/*
+ * 
+ * Copyright (C) 2016 guru
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+package com.linux.rhcloud.javaee.movieinfo.business.country;
+
+import static com.linux.rhcloud.javaee.movieinfo.business.country.Country.GET_ALL_COUNTRIES;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Guruprasad Kulkarni <guru@linux.com>
+ */
+@Entity(name = "country")
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@NamedQueries(@NamedQuery(name = GET_ALL_COUNTRIES, query = "SELECT c FROM ccountry c"))
+public class Country implements Serializable {
+    
+    private static final long serialVersionUID = 2456594345117210500L;
+    
+    private static final String PREFIX = "movieinfo.business.country.";
+
+    /**
+     * Named Query :: {@value #GET_ALL_CITIES} to get all actors
+     */
+    public static final String GET_ALL_COUNTRIES = PREFIX + "findAll";
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "country_id")
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String country;
+    
+    @NotNull
+    @Column(name = "last_update")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastUpdate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+    
+    
+}
