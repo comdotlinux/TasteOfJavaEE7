8cb29e318519c169e445aeb0e3c79fd05709dba1
Guruprasad Kulkarni <guru@linux.com>
2016-05-26 09:04:43 +0530

    In Progress Performance monitoring -- untested

--- movieInfo/src/main/java/com/linux/rhcloud/javaee/movieinfo/business/monitoring/boundry/MonitorSink.java
+++ movieInfo/src/main/java/com/linux/rhcloud/javaee/movieinfo/business/monitoring/boundry/MonitorSink.java
@@ -20,7 +20,9 @@ package com.linux.rhcloud.javaee.movieinfo.business.monitoring.boundry;
 
 import com.linux.rhcloud.javaee.movieinfo.business.monitoring.entity.CallEvent;
 import java.util.List;
+import java.util.LongSummaryStatistics;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.ejb.ConcurrencyManagement;
 import javax.ejb.ConcurrencyManagementType;
@@ -35,26 +37,27 @@ import javax.json.JsonArray;
 @Singleton
 @ConcurrencyManagement(ConcurrencyManagementType.BEAN)
 public class MonitorSink {
-    
+
     CopyOnWriteArrayList<CallEvent> events;
-    
+
     @PostConstruct
     public void init() {
         events = new CopyOnWriteArrayList<>();
     }
-    
+
     public void onEvent(@Observes CallEvent callEvent) {
-        if(events.size() > 30000) {
+        if (events.size() > 30000) {
             events.clear();
         }
         events.add(callEvent);
     }
-    
-    
+
     public List<CallEvent> getEvents() {
         return events;
     }
-    
-    
-    
+
+    public LongSummaryStatistics getStatistics() {
+        return events.stream().collect(Collectors.summarizingLong(CallEvent::getDurationInMillis));
+    }
+
 }
